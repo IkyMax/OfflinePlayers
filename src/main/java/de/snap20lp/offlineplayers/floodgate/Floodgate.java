6438546ffb6ef13a5bd340633936a3d84a8e3ee3
IkyMax <112127881+IkyMax@users.noreply.github.com>
2023-10-25 02:59:15 -0600

    Create Floodgate.java

---
+++ src/main/java/de/snap20lp/offlineplayers/floodgate/Floodgate.java
@@ -0,0 +1,13 @@
+/* Original by ofunny */
+
+package de.snap20lp.offlineplayers.floodgate;
+
+import org.bukkit.entity.Player;
+
+public interface Floodgate {
+	
+	public boolean isBedrockPlayer(Player player);
+
+}
+
+// end Floodgate
